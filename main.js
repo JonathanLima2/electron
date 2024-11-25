abad1f9b8f4ff83af5b37be9044bac0d41dc9a6c
JonathanLima2 <jdsl@icomp.ufam.edu.br>
2024-11-25 09:24:56 -0400

    feat: new electron app test

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('first test to see package function')
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+function createWindow(){
+    const mainWindow = new BrowserWindow({
+        title:'Electron',
+        width:1000,
+        height:600,
+    });
+    const starUrl= url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+    });
+    mainWindow.loadURL(starUrl);
+}
+
+app.whenReady().then(createWindow);
